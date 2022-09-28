e3e1d63759e2cc6406555f2b7b206b97382d4bc6
Iket <112098857+Idfbkw@users.noreply.github.com>
2022-09-28 11:03:44 +0800

    Create 查看数组长度和字符串长度.java

---
+++ 查看数组长度和字符串长度.java
@@ -0,0 +1,19 @@
+package abcd;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*private static viod getArea(){
+			double radius=5.5;
+			double yuanmi=3.14*radius*radius;
+			System.out.println{"圆面积为："+园}；
+		}*/
+		int[] y=new int[100];
+		System.out.println(y.length);
+		String str1="nihao";
+		System.out.println(str1.length());
+				
+	}
+
+}
